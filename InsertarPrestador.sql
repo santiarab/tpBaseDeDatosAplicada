22175e04db36ab83ebf04a1834b68d6b25cd8211
santiarab <santiarab7@gmail.com>
2024-06-09 18:13:32 -0300

    first commit

--- InsertarPrestador.sql
+++ InsertarPrestador.sql
@@ -1,20 +1,14 @@
-create table prestador
-(
-	id_prestador int identity(1,1),
-	nombre_prestador varchar(40) not null,
-	plan_prestador varchar(30) not null,
-	constraint pk_prestador primary key (id_prestador)
-)
-go
-drop table prestador
-CREATE OR ALTER PROCEDURE insertarPrestador
+CREATE OR ALTER PROCEDURE procedimientos.insertar_prestador
     @nombre_prestador varchar(40),@plan_prestador varchar(30)
 AS
 begin
 	SET NOCOUNT ON;
 	set @nombre_prestador = LOWER(LTRIM(RTRIM(@nombre_prestador)))
 	set @plan_prestador = LOWER(LTRIM(RTRIM(@plan_prestador)))
-	if NOT EXISTS (select 1 from prestador where nombre_prestador COLLATE Latin1_General_CI_AS = @nombre_prestador COLLATE Latin1_General_CI_AS)
+	if NOT EXISTS (select 1 from tabla.prestador where nombre_prestador COLLATE Latin1_General_CI_AS = @nombre_prestador COLLATE Latin1_General_CI_AS 
+	AND 
+	plan_prestador COLLATE Latin1_General_CI_AS = @plan_prestador COLLATE Latin1_General_CI_AS
+	)
 	begin
 		insert into prestador(nombre_prestador,plan_prestador) values(@nombre_prestador,@plan_prestador)
 		PRINT ''
